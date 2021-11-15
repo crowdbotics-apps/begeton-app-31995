29902e8fab50d440b9d06c725d5ef56c7df227ce
Crowdbotics <team@crowdbotics.com>
2021-11-15 14:22:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/begeton_app_31995/MainActivity.java
@@ -0,0 +1,15 @@
+package com.begeton_app_31995;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "begeton_app_31995";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "begeton_app_31995",
+  url: "https://begeton_app_31995.botics.co",
+  api: "https://begeton_app_31995.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
